26187e3c9f34d09fb4de444a558527e0b78ff781
FM <franz.mamani@qhanati.com>
2024-08-28 11:02:54 -0400

    create Basic Proyect

---
+++ noteapp/noteapp Watch App/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  noteapp Watch App
+//
+//  Created by QHANATI on 8/28/24.
+//
+
+import SwiftUI
+
+struct BtnAcctions: View {
+    var body: some View{
+        Text("Hello")
+    }
+}
+
+struct ContentView: View {
+    @State var peackerVal = 5;
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button(
+                action: {
+//                    BtnAcctions()
+                },
+                label: {
+                   /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                }
+            )
+            Picker(selection: $peackerVal, label: Text("")) {
+                Text("1").tag(1)
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+                Text("20").tag(20)
+                Text("25").tag(25)
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ noteapp/noteapp Watch App/noteappApp.swift
@@ -0,0 +1,17 @@
+//
+//  noteappApp.swift
+//  noteapp Watch App
+//
+//  Created by QHANATI on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct noteapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
