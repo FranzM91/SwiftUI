ab58a51d77f211cca3d35016d2e02bdd61b423f1
FM <franz.mamani@qhanati.com>
2024-08-29 14:55:27 -0400

    added new base project

---
+++ HelloSwift.playground/Contents.swift
@@ -0,0 +1,5 @@
+//import UIKit //-- se utiliza para traer el kit de interfaces
+import PlaygroundSupport
+
+var result = "Hello, playground"
+print(result)
--- noteapp/noteapp Watch App/ContentView.swift
+++ noteapp/noteapp Watch App/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Qhanati!")
             Button(
                 action: {
 //                    BtnAcctions()
